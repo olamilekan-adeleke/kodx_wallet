22a4e3a85179a553f2f6bef70b3db156e5e2d7c7
olamilekan-adeleke <olamilekanadeleke123@gmail.com>
2021-11-04 22:07:35 +0100

    changes

--- src/kodx_wallet_backend/controllers/user/get_search_user.js
+++ src/kodx_wallet_backend/controllers/user/get_search_user.js
@@ -2,8 +2,29 @@ const {
   userCollectionRef,
 } = require("../../controllers/firebase/firebase_admin");
 
-const getSearchUser = (lastUserId) => {
-    
+const getSearchUser = async (searchQuery, lastUserId) => {
+  let user = [];
+
+  if (!lastUserId) {
+    const query = await userCollectionRef
+      .orderBy("username")
+      .where("username", "array-contains", searchQuery)
+      .limit(10)
+      .get();
+
+    users = query.docs.map((doc) => doc.data());
+  } else {
+    const query = await userCollectionRef
+      .orderBy("username")
+      .where("username", "array-contains", searchQuery)
+      .startAfter(lastUserId)
+      .limit(10)
+      .get();
+
+    users = query.docs.map((doc) => doc.data());
+  }
+
+  console.log(users);
 };
 
 module.exports = getSearchUser;
--- src/kodx_wallet_backend/routes/user/search_users.js
+++ src/kodx_wallet_backend/routes/user/search_users.js
@@ -3,9 +3,25 @@ const getSearchUser = require("../../controllers/user/get_search_user");
 const searchUsers = async (req, res) => {
   try {
     const userId = req.decoded.id;
-    const { last_user_id } = req.body;
+    const { last_user_id, query } = req.query;
 
-    const users = await getSearchUser(last_user_id);
+    console.log(query);
+
+    if (!query) {
+      throw {
+        code: 400,
+        msg: "Search Query Needed!!",
+      };
+    }
+
+    const users = await getSearchUser(query, last_user_id);
+
+    if (users === undefined || users.length === 0) {
+      throw {
+        code: 404,
+        msg: "User not found!",
+      };
+    }
 
     res.status(200).json({
       status: "success",
@@ -23,3 +39,5 @@ const searchUsers = async (req, res) => {
 };
 
 module.exports = searchUsers;
+
+// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjEwMzI2NjU1LWIxMjUtNDM4MS1hNTk1LTE5NTRkMjEyNjdiOSIsImlhdCI6MTYzNjA1NzgzMywiZXhwIjoxNjM2MDY1MDMzfQ.iTHo6H9hnMflPiHPP70HtasUqVnFZ0igdPfj-Jq__hM
