38a839dc9ff85a512aad2c17007ffd64046f78f7
olamilekan-adeleke <olamilekanadeleke123@gmail.com>
2021-11-05 07:12:26 +0100

    changes

--- src/kodx_wallet_backend/controllers/wallet/validate_transaction.js
+++ src/kodx_wallet_backend/controllers/wallet/validate_transaction.js
@@ -1,3 +1,12 @@
-const validateTransactionFormPayStack = (userId, amount) => {};
+const validateTransactionFormPayStack = async (txId) => {
+  try {
+    return 100;
+  } catch (err) {
+    throw {
+      code: 400,
+      msg: "Unable To Confirm Transaction!",
+    };
+  }
+};
 
 module.exports = validateTransactionFormPayStack;
--- src/kodx_wallet_backend/routes/wallet/fund_wallet.js
+++ src/kodx_wallet_backend/routes/wallet/fund_wallet.js
@@ -1,8 +1,15 @@
+const validateTransactionFormPayStack = require("../../controllers/wallet/validate_transaction");
+
 const fundWallet = (req, res) => {
   try {
+    const userId = req.decoded.id;
+    const txId = req.query.txId;
+
     // check pay-stack to confirm transaction
+    const amount = await validateTransactionFormPayStack(txId);
 
     // update user wallet by that amount
+      
 
     // add transaction  to transaction history
     res.status(200).json({
