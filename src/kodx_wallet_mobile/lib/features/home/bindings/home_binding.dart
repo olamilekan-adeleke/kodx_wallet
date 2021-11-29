ab56f4dad462554fd4117d8a5e85939514d08c28
olamilekan-adeleke <olamilekanadeleke123@gmail.com>
2021-11-29 10:00:42 +0100

    update

--- src/kodx_wallet_mobile/lib/features/home/bindings/home_binding.dart
+++ src/kodx_wallet_mobile/lib/features/home/bindings/home_binding.dart
@@ -1,9 +1,11 @@
 import 'package:get/get.dart';
 import 'package:kodx_wallet_mobile/features/home/controller/get_user_details_controller.dart';
+import 'package:kodx_wallet_mobile/features/home/controller/wallet_controller.dart';
 
 class HomeBindings extends Bindings {
   @override
   void dependencies() {
     Get.put<GetUserDetailsController>(GetUserDetailsController());
+    Get.put<WalletController>(WalletController());
   }
 }
---
+++ src/kodx_wallet_mobile/lib/features/home/services/wallet_services.dart
@@ -0,0 +1,46 @@
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:get/instance_manager.dart';
+import 'package:http/http.dart' as http;
+import 'package:kodx_wallet_mobile/cores/utils/api.dart';
+import 'package:kodx_wallet_mobile/features/auth/model/user_details_model.dart';
+import 'package:kodx_wallet_mobile/features/auth/services/auth_services.dart';
+
+class WalletService {
+  final AuthenticationRepo authenticationRepo = Get.find<AuthenticationRepo>();
+
+  Future<UserDetailsModel?> getWalletData() async {
+    UserDetailsModel? result;
+
+    final Map<String, String> header = {
+      "Content-Type": "application/json",
+      "Authorization": "Bearer ${authenticationRepo.token}",
+    };
+
+    http.Response response = await http.get(
+      Uri.parse(ApiEndpoints.getWallet),
+      headers: header,
+    );
+
+    log(response.body);
+
+    if (response.statusCode >= 500) {
+      throw 'Unable to connect to server! \nSomething went wrong.';
+    }
+
+    final Map<String, dynamic> responseData = json.decode(response.body);
+
+    if (response.statusCode == 200 || response.statusCode == 201) {
+      if (responseData['status'] == 'success') {
+        result = UserDetailsModel.fromMap(responseData['data']);
+      } else {
+        throw responseData['msg'];
+      }
+    } else {
+      throw responseData['msg'];
+    }
+
+    return result;
+  }
+}
