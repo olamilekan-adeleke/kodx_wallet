32a0e351aa8e9bfce959ff7c441041662d7c4182
olamilekan-adeleke <olamilekanadeleke123@gmail.com>
2021-11-29 11:45:43 +0100

    update

--- src/kodx_wallet_mobile/lib/features/home/views/screens/home_screen.dart
+++ src/kodx_wallet_mobile/lib/features/home/views/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:kodx_wallet_mobile/cores/utils/custom_sizer_utils.dart';
 import 'package:kodx_wallet_mobile/features/home/views/widgets/home_screen_header.dart';
+import 'package:kodx_wallet_mobile/features/home/views/widgets/home_wallet_widget.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -11,6 +12,8 @@ class HomeScreen extends StatelessWidget {
       children: <Widget>[
         SizedBox(height: sizerSp(15)),
         const HomeScreenHeaderWidget(),
+        SizedBox(height: sizerSp(20)),
+        const HomeWalletWidget(),
         Container(),
       ],
     );
---
+++ src/kodx_wallet_mobile/lib/features/home/views/widgets/home_wallet_widget.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:kodx_wallet_mobile/cores/components/custom_text_widget.dart';
+import 'package:kodx_wallet_mobile/cores/constants/color.dart';
+import 'package:kodx_wallet_mobile/cores/utils/custom_sizer_utils.dart';
+
+class HomeWalletWidget extends StatelessWidget {
+  const HomeWalletWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(sizerSp(10)),
+      height: sizerSp(150),
+      width: double.infinity,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(sizerSp(8)),
+        gradient: LinearGradient(
+          colors: [
+            kcPrimaryColor,
+            kcPrimaryColor.withOpacity(0.8),
+            kcPrimaryColor.withOpacity(0.7),
+          ],
+        ),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              textWidget(
+                'Total Balance',
+                color: white.withOpacity(0.8),
+                fontWeight: FontWeight.w400,
+                size: sizerSp(14),
+              ),
+              textWidget(
+                '\$ 56,684.87',
+                color: white,
+                size: sizerSp(22),
+                fontWeight: FontWeight.w500,
+              ),
+            ],
+          ),
+          const Spacer(),
+          Row(
+            children: <Widget>[
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  textWidget(
+                    'Expenses',
+                    color: white.withOpacity(0.8),
+                    fontWeight: FontWeight.w400,
+                    size: sizerSp(12),
+                  ),
+                  textWidget(
+                    '\$ 6,243.55',
+                    color: white,
+                    size: sizerSp(19),
+                    fontWeight: FontWeight.w500,
+                  ),
+                ],
+              ),
+              const Spacer(),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  textWidget(
+                    'Income',
+                    color: white.withOpacity(0.8),
+                    fontWeight: FontWeight.w400,
+                    size: sizerSp(12),
+                  ),
+                  textWidget(
+                    '\$ 10,504.23',
+                    color: white,
+                    size: sizerSp(19),
+                    fontWeight: FontWeight.w500,
+                  ),
+                ],
+              ),
+              SizedBox(width: sizerSp(40)),
+            ],
+          ),
+          SizedBox(height: sizerSp(20)),
+        ],
+      ),
+    );
+  }
+}
