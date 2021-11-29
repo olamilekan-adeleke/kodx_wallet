488f8b68526c69faa108b021493e471d704c4071
olamilekan-adeleke <olamilekanadeleke123@gmail.com>
2021-11-29 11:15:48 +0100

    update

--- src/kodx_wallet_mobile/lib/features/home/views/widgets/home_tab_body_widget.dart
+++ src/kodx_wallet_mobile/lib/features/home/views/widgets/home_tab_body_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:kodx_wallet_mobile/features/home/controller/home_controller.dart';
+import 'package:kodx_wallet_mobile/features/home/views/screens/home_screen.dart';
 
 Widget homeTabBodyWidget() {
   final HomeController homeController = Get.find<HomeController>();
@@ -9,7 +10,7 @@ Widget homeTabBodyWidget() {
     return IndexedStack(
       index: homeController.tabIndex.value,
       children: [
-        Container(child: Center(child: Text('home'))),
+        const HomeScreen(),
         Container(child: Center(child: Text('transer'))),
         Container(child: Center(child: Text('transaction'))),
         Container(child: Center(child: Text('profile'))),
